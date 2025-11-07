8413213276d7bf9663069a0569bb6dbc99bd449f
Hamim18 <hamimltf18@gmail.com>
2025-11-07 18:34:22 +0700

    latihan java sederhana, menampilkan nim, nama, dll

---
+++ latihan java/src/latihan1.java
@@ -0,0 +1,12 @@
+// membuaat program sederhana dengan output 
+// nim, nama, angkatan, ipk, dan prodi
+public class latihan1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nim : 256612004");
+        System.out.println("Nama : Hamim Abdul Latif");
+        System.out.println("Angkatan : 2025");
+        System.out.println("IPK : 4");
+        System.out.println("Prodi : Teknik Informatika");
+    }
+}
